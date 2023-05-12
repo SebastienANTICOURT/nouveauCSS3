1a61868698c39a69ee86fb4e159a28c361a8301b
SebastienANTICOURT <sebastien.anticourt@live.fr>
2023-05-12 11:31:36 +0200

    exemple

---
+++ script.js
@@ -0,0 +1,22 @@
+/*console.log("Hello, World");
+let myName = "Seb";
+console.log(myName);
+myName = "tom";
+console.log(myName);
+const name = "David";
+const lastname = "bruno";
+let presentation = "Hi, I'm David Bruno from SF, I like to cook and meet new people.";
+let profilpicture = "http://www.go.com/davif.png";
+let followers = 109;
+let following = 200;
+following = following + 1;
+console.log(following);|*/
+const firstname ="Paul";
+const password = "chanel"
+if(firstname === "Paul" && password ==="chanel"){
+    console.log("welcome" + firstname)
+}else{
+    console.log("Go away!!");
+}
+const user= prompt("truc")
+console.log(user)

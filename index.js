b6b3300ae49313972d71d98227333d8acea7c599
SebastienANTICOURT <sebastien.anticourt@live.fr>
2023-06-27 12:02:07 +0200

    exo node04

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-const f = require("./CSS3");
+// const f = require("./CSS3");
 
-console.log(f);
+// console.log(f);
 
-const {cows} = require ("cowsay");
+// const {cows} = require ("cowsay");
 
-var cowsay = require("cowsay");
+// var cowsay = require("cowsay");
 
-console.log(cowsay.say({
-    text : "I'm a moooodule",
-  }));
+// console.log(cowsay.say({
+//     text : "I'm a moooodule",
+//   }));
+
+require('dotenv').config();
+
+const myName = process.env.MY_NAME;
+const myCity = process.env.MY_CITY;
+const myLanguage = process.env.MY_LANGUAGE;
+
+const sentence = `I am ${myName}, a student in ${myCity}, and I love ${myLanguage}.`;
+
+console.log(sentence);
